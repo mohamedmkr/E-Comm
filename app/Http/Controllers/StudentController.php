dc25bd8fbee2b6c3f22a85687e1e66ba5b8f65f2
Bayan-Daleel <bayan.dl96@gmail.com>
2023-03-10 19:42:04 +0300

    modify function body

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,16 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Course;
 use App\Models\Post;
+use App\Models\Course;
+use App\Models\Student;
 use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 
 class StudentController extends Controller
 {
       public function showPostView(){
-        return view('student.postCommentView');
+
+        $posts=Comment::findAll();
+        return view('student.postComment',compact('posts'));
     }
 
 
@@ -22,18 +26,22 @@ class StudentController extends Controller
         ]);
         // Eloquent Model
         Comment::create($request->comment());
-        return redirect()->route('home.Homeview')->with('success', 'comment Added Successfully');}
+        return redirect()->route('home')->with('success', 'comment Added Successfully');}
 
 
     public function registerInCourse($course_id){
+        $id = Auth::id();
+        $student = Student::where('id', $id);
+        $student->course()->attach($course_id);
 
-       return view('student.CoursesView', [
-            'course' =>Course::findOrFail($course_id)
-        ]);
+       return view('student.Courses');
     }
 
     public function showEnrolledCourses(){
-        return view('student.CoursesView');
+
+        $id = Auth::id();
+        $courses=Student::where('id', $id)->courses();
+        return view('student.Courses',compact('courses'));
 
 
     }
