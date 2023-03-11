65da8dfa88bcfd6878e9878260182eab6837f147
Badr Akbik <badrakbik1@gmail.com>
2023-03-11 22:24:55 +0200

    making middlewares

---
+++ app/Http/Middleware/checkIsStudent.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class checkIsStudent
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->check() && auth()->user()->getTable() == 'students')
+        return $next($request);
+    else{
+        return redirect('/login');
+    }
+    }
+}
